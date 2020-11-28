6e3928d3081715120863d219fc8ffc82e827e7b9
石志鹏 <3479603121@qq.com>
2020-11-28 15:53:26 +0800

    header

---
+++ src/components/content/home/childComps/HomeHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav-bar class="home-header">
+    <div slot="left" class="iconfont">&#xe600;</div>  
+    <div slot="center" class="iconfont center"> &#xe670;请输入城市/景点/游玩主题</div>  
+    <div slot="right" class="iconfont left">北京&#xe601;</div> 
+  </nav-bar>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar'
+
+export default {
+  name:'HomeHeader',
+  components:{
+    NavBar
+  }
+}
+</script>
+
+<style scoped>
+
+  .home-header{
+    width: 100%;
+    height: 44px;
+    background: #00bcd4;
+    color: #fff;
+  }
+
+  .center{
+    background: #fff;
+    height: 30px;
+    line-height: 30px;
+    font-size: 14px;
+    margin: 7px 0;
+    color: #e4e7ea;
+    text-align: left;
+    padding-left: 10px;
+    border-radius: 5px;
+  }
+
+  .left{
+    text-align: center;
+    font-size: 14px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+//css
+import 'assets/css/reset.css'
+import 'assets/css/iconfont.css'
+
+//js(解决移动端300ms延迟问题) 
+import fastClick from 'fastclick'
+fastClick.attach(document.body);
+
 new Vue({
   router,
   render: h => h(App)
