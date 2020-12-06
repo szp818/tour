7082c6b878b37959e2e6354aad5b7c619577da38
石志鹏 <3479603121@qq.com>
2020-12-06 22:46:05 +0800

    add location

--- src/components/content/home/Home.vue
+++ src/components/content/home/Home.vue
@@ -3,6 +3,7 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-location></home-location>
   </div>
 </template>
 
@@ -10,13 +11,15 @@
 import HomeSwiper from "./childComps/HomeSwiper"
 import HomeHeader from './childComps/HomeHeader'
 import HomeIcons  from './childComps/HomeIcons'
+import HomeLocation from './childComps/HomeLocation'
 
 export default {
     name:'Home',
     components:{
       HomeHeader,
       HomeSwiper,
-      HomeIcons
+      HomeIcons,
+      HomeLocation
     }
 }
 </script>
---
+++ src/components/content/home/childComps/HomeLocation.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home-location border-top">
+    <div class="home-location-item border-right">
+      <span class="iconfont">&#xe608;</span>
+      定位失败
+    </div>
+     <div class="home-location-item">
+      <span class="iconfont">&#xe602;</span>
+      必游榜单
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'HomeLocation'
+}
+</script>
+
+<style>
+  .home-location{
+    display: flex;
+    position: relative;
+  }
+  .home-location-item{
+    flex: 1;
+    font-size:  14px;
+    text-align: center;
+    height: 50px;
+    line-height: 50px;
+  }
+</style>
